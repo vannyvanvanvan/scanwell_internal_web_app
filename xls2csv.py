d71bc93e8451975a10c90e997a06e15d1b02d263
swinghin <swinghin@hotmail.com>
2024-11-19 16:01:18 +0000

    Check csv column value validity

--- xls2csv.py
+++ xls2csv.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas as pd
 
 xlsx_name = "美加航線倉位狀況表20241018.xlsx"
@@ -20,38 +21,53 @@ def is_csv_valid(file, extension="csv"):
             else pd.read_excel(file, keep_default_na=False)
         )
 
-        print(df.columns)
+        # columns required
+        target_columns = {
+            "CS": str,
+            "WEEK": int,
+            "CARRIER": str,
+            "SERVICE": str,
+            "M/V": str,
+            "S/O": str,
+            "SIZE": str,
+            "POL": str,
+            "POD": str,
+            "FINAL DEST": str,
+            "ROUTING": str,
+            "CY OPEN": datetime,
+            "SI CUT OFF": datetime,
+            "CY/CV CLS": datetime,
+            "ETD": datetime,
+            "ETA": datetime,
+            "Contract/Co-loader": str,
+            "S/": str,
+            "C/": str,
+            "TERM": str,
+            "Saleman": str,
+            "Cost": float,
+            "RATE VALID": datetime,
+            "S/R": float,
+            "Remark": str,
+        }
 
-        target_columns = [
-            "CS",
-            "WEEK",
-            "CARRIER",
-            "SERVICE",
-            "M/V",
-            "S/O",
-            "SIZE",
-            "POL",
-            "POD",
-            "FINAL DEST",
-            "ROUTING",
-            "CY OPEN",
-            "SI CUT OFF",
-            "CY/CV CLS",
-            "ETD",
-            "ETA",
-            "Contract/Co-loader",
-            "S/",
-            "C/",
-            "TERM",
-            "Saleman",
-            "Cost",
-            "RATE VALID",
-            "S/R",
-            "Remark",
-        ]
+        # throw error with missing columns
+        columns_missing = []
+        for target in target_columns.keys():
+            if target not in df.columns:
+                columns_missing.append(target)
+        if len(columns_missing) > 0:
+            raise ValueError("Missing columns %s", ", ".join(columns_missing))
 
-        if not all(column in target_columns for column in df.columns):
-            raise ValueError("Incomplete column data")
+        # loop all column data to check data type
+        for column in df.columns:
+            column_type = target_columns[column]
+            column_values = df[column].tolist()
+
+            # check all values of current column
+            for value in column_values:
+                # if value is not blank and type mismatch, try parsing (i.e. datetime from int)
+                if not value == "" and not column_type == type(value):
+                    column_type(value)
 
     except Exception as e:
         print(e)
@@ -59,4 +75,4 @@ def is_csv_valid(file, extension="csv"):
     return True
 
 
-print(is_csv_valid(xlsx_name, "xlsx"))
+print("Valid" if is_csv_valid(xlsx_name, "xlsx") else "Invalid")
