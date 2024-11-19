8e148b2de0709bd974b13356e80d5ee9fdf34654
swinghin <swinghin@hotmail.com>
2024-11-19 13:11:42 +0000

    Check csv column validity

--- xls2csv.py
+++ xls2csv.py
@@ -9,12 +9,52 @@ csv_name = "xls2.csv"
 
 def is_csv_valid(file, extension="csv"):
     try:
+        # file not valid if extension incorrect
+        if extension not in ["csv", "xlsx"]:
+            raise ValueError("File format not recognised")
+
+        # read dataframe from file name based on extension
         df = (
             pd.read_csv(file, keep_default_na=False)
             if extension == "csv"
             else pd.read_excel(file, keep_default_na=False)
         )
+
+        print(df.columns)
+
+        target_columns = [
+            "CS",
+            "WEEK",
+            "CARRIER",
+            "SERVICE",
+            "M/V",
+            "S/O",
+            "SIZE",
+            "POL",
+            "POD",
+            "FINAL DEST",
+            "ROUTING",
+            "CY OPEN",
+            "SI CUT OFF",
+            "CY/CV CLS",
+            "ETD",
+            "ETA",
+            "Contract/Co-loader",
+            "S/",
+            "C/",
+            "TERM",
+            "Saleman",
+            "Cost",
+            "RATE VALID",
+            "S/R",
+            "Remark",
+        ]
+
+        if not all(column in target_columns for column in df.columns):
+            raise ValueError("Incomplete column data")
+
     except Exception as e:
+        print(e)
         return False
     return True
 
