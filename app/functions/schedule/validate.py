01086133e7f7f95669958b8b5413816dbe624094
swinghin <swinghin@hotmail.com>
2025-01-10 16:13:01 +0000

    Document validator functions

--- app/functions/schedule/validate.py
+++ app/functions/schedule/validate.py
@@ -2,22 +2,27 @@ from datetime import datetime
 
 
 def is_valid_string(string: str) -> bool:
+    # Return True/False if string is valid alphamumeric
     return string.isidentifier()
 
 
 def is_valid_number(number: str) -> bool:
+    # Return True/False if string is valid number
     return number.isdigit()
 
 
 def zero_or_valid_number(check_number: str) -> int:
+    # Return 0 if number is not valid, else return parsed integer
     return 0 if not is_valid_number(check_number) else int(check_number)
 
 
 def is_valid_week(week: str) -> bool:
+    # Return True if week is parsable integer and within 1-52, else False
     return is_valid_number(week) and int(week) > 0 and int(week) <= 52
 
 
 def now_or_valid_week(check_week: str) -> int:
+    # Return today's week if week input invalid, else return valid week input
     return (
         datetime.now().isocalendar().week
         if not is_valid_week(check_week)
@@ -26,10 +31,12 @@ def now_or_valid_week(check_week: str) -> int:
 
 
 def default_or_valid_week(default_week: int, check_week: str) -> int:
+    # Return first parameter if second parameter is invalid week
     return default_week if not is_valid_number(check_week) else int(check_week)
 
 
 def is_valid_date(date: str) -> bool:
+    # Return True/False if string can be parsed as date
     try:
         datetime.strptime(date, "%Y-%m-%d")
         return True
@@ -37,7 +44,8 @@ def is_valid_date(date: str) -> bool:
         return False
 
 
-def now_or_valid_date(check_date: str) -> str:
+def now_or_valid_date(check_date: str) -> datetime:
+    # Return today's datetime if string not valid date, else return parsed datetime
     return (
         datetime.now()
         if not is_valid_date(check_date)
@@ -46,6 +54,7 @@ def now_or_valid_date(check_date: str) -> str:
 
 
 def default_or_valid_date(default_date: datetime, check_date: str) -> datetime:
+    # Return first parameter if second parameter cannot be parsed as date, else return second parameter
     return (
         default_date
         if not is_valid_date(check_date)
@@ -54,6 +63,7 @@ def default_or_valid_date(default_date: datetime, check_date: str) -> datetime:
 
 
 def is_valid_datetime(date: str, time: str) -> bool:
+    # Return True/False if string can be parsed as datetime
     try:
         datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M")
         return True
@@ -62,6 +72,7 @@ def is_valid_datetime(date: str, time: str) -> bool:
 
 
 def now_or_valid_datetime(check_date: str, check_time: str) -> datetime:
+    # Return now datetime if string not valid datetime, else return parsed datetime
     return (
         datetime.now()
         if not is_valid_datetime(check_date, check_time)
@@ -72,6 +83,7 @@ def now_or_valid_datetime(check_date: str, check_time: str) -> datetime:
 def default_or_valid_datetime(
     default_datetime: datetime, check_date: str, check_time: str
 ) -> datetime:
+    # Return first parameter if second parameter cannot be parsed as datetime, else return second parameter
     return (
         default_datetime
         if not is_valid_datetime(check_date, check_time)
@@ -80,6 +92,7 @@ def default_or_valid_datetime(
 
 
 def is_valid_schedule_dict(form: dict) -> bool:
+    # Check if dictionary contains all keys for schedule data
     return all(
         item in form
         for item in [
@@ -103,6 +116,7 @@ def is_valid_schedule_dict(form: dict) -> bool:
 
 
 def is_valid_schedule_form(form: dict) -> bool:
+    # Return True/False if all key values are valid
     return (
         is_valid_schedule_dict(form)
         and is_valid_string(form["cs"])
