bed94ad2498ea085b5448ff1c2972dbf1edce719
Man Hei Ong <vanessaaaaong@gmail.com>
2025-01-20 18:47:22 +0000

    - Added saved locked time in the database

--- app/model.py
+++ app/model.py
@@ -40,6 +40,7 @@ class LoginStatus(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
     status = db.Column(db.String(50), nullable=False, default='offline')
     failed_attempts = db.Column(db.Integer, default=0)
+    locked_until = db.Column(db.DateTime, nullable=True)
     last_login = db.Column(db.DateTime, nullable=True)
     last_logoff = db.Column(db.DateTime, nullable=True)
     ip_connected = db.Column(db.String(100), nullable=True)
