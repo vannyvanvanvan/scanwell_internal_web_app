439234d148d49454c9cb970980729af42eb9bdc4
Man Hei Ong <vanessaaaaong@gmail.com>
2025-01-20 16:07:33 +0000

    - Updated logout

--- app/functions/user/logout.py
+++ app/functions/user/logout.py
@@ -1,8 +1,13 @@
+from datetime import datetime
 from flask import flash, redirect, url_for
-from flask_login import logout_user
-
+from flask_login import current_user, logout_user
+from app.model import db
 
 def logout_page():
+    if current_user.is_authenticated:
+        if current_user.login_status:
+            current_user.login_status.last_logoff = datetime.utcnow()
+            db.session.commit()
     logout_user()
     flash("You have successfully logged out.", "success")
     return redirect(url_for("user.user_login"))
