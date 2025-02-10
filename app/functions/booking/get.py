76dd58e2d5e61113e7e74e2780968ffcbc804dd0
swinghin <swinghin@hotmail.com>
2025-02-10 20:33:09 +0000

    Change method to find sales booking

--- app/functions/booking/get.py
+++ app/functions/booking/get.py
@@ -5,11 +5,11 @@ from app.model import Booking
 
 
 def get_sales_booking(userid: int) -> list:
-    sales_spc_ids = [reserve.spc_id for reserve in get_sales_reserve(userid)]
-    return Booking.query.filter(Booking.spc_id.in_(sales_spc_ids)).all()
+    sales_rsv_ids = [reserve.rsv_id for reserve in get_sales_reserve(userid)]
+    return Booking.query.filter(Booking.rsv_id.in_(sales_rsv_ids)).all()
+
 
 def booking_table_results(reserves: list) -> str:
     return render_template(
         "reserve_table_results.html", current_user=current_user, reserves=reserves
     )
-
