009f5d4ad1cee49fd4adb4addeb19c24e2323587
Vanny <vanessaaaaong@gmail.com>
2025-08-19 21:13:29 +0800

    - CS can confirm/reject their booking

--- app/functions/booking/action.py
+++ app/functions/booking/action.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-from flask import flash, redirect, url_for
+from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user
+from app.functions.user.get import get_all_users_names_dict
+from sqlalchemy.exc import SQLAlchemyError
 from app.model import Booking, Space, db
 
 def pending_booking(spc_id: int) -> bool:
@@ -17,4 +19,84 @@ def pending_booking(spc_id: int) -> bool:
     
     except Exception as e:
         flash(f'Error updating booking status: {str(e)}', 'error')
+        return False
+    
+def confirm_booking_page(bk_id: int) -> str:
+    try:
+        booking = Booking.query.get_or_404(bk_id)
+        space = Space.query.get(booking.spc_id)
+        users = get_all_users_names_dict()
+        
+        return render_template(
+            "shipping_booking_space.html",
+            booking=booking,
+            space=space,
+            users=users,
+            data=booking,
+            page_title="Booking Confirm"
+        )
+        
+    except Exception as e:
+        flash(f'Error loading booking: {str(e)}', 'error')
+        return redirect(url_for('user.user_home'))
+
+def confirm_booking(bk_id: int):
+    try:
+        booking = Booking.query.get_or_404(bk_id)
+        if 'remark' in request.form:
+            booking.remark = request.form['remark']
+
+        space = Space.query.get(booking.spc_id)
+        space.spcstatus = 'BK_CONFIRM'
+        space.last_modified_by = current_user.id
+        space.last_modified_at = datetime.utcnow()
+        
+        db.session.commit()
+        flash('Booking confirmed successfully!', 'success')
+        return redirect(url_for('user.user_home'))
+    
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        flash(f"Database error: {str(e)}", "danger")
+        return False
+
+def decline_booking_page(bk_id: int) -> str:
+    try:
+        booking = Booking.query.get_or_404(bk_id)
+        space = Space.query.get(booking.spc_id)
+        users = get_all_users_names_dict()
+        
+        return render_template(
+            "shipping_booking_space.html",
+            booking=booking,
+            space=space,
+            users=users,
+            data=booking,
+            page_title="Booking Reject"
+            
+        )
+        
+    except Exception as e:
+        flash(f'Error loading booking: {str(e)}', 'error')
+        return redirect(url_for('user.user_home'))
+
+def decline_booking(bk_id: int):
+    try:
+        booking = Booking.query.get_or_404(bk_id)
+        if 'remark' in request.form:
+            booking.remark = request.form['remark']
+
+        booking.void = True
+        space = Space.query.get(booking.spc_id)
+        space.spcstatus = 'USABLE'
+        space.last_modified_by = current_user.id
+        space.last_modified_at = datetime.utcnow()
+        
+        db.session.commit()
+        flash('Booking declined successfully!', 'success')
+        return redirect(url_for('user.user_home'))
+    
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        flash(f"Database error: {str(e)}", "danger")
         return False
--- app/routes/booking.py
+++ app/routes/booking.py
@@ -1,4 +1,4 @@
-from app.functions.booking.action import pending_booking
+from app.functions.booking.action import confirm_booking, confirm_booking_page, decline_booking, decline_booking_page, pending_booking
 from flask_login import login_required
 from app.functions.booking.delete import delete_booking
 from app.functions.booking.edit import edit_booking, edit_booking_page
@@ -7,7 +7,9 @@ from app.functions.permissions import role_required
 from flask import (
     flash,
     Blueprint,
+    redirect,
     request,
+    url_for,
 )
 
 
@@ -24,7 +26,6 @@ booking_routes = Blueprint(
 @role_required(["admin", "cs"])
 def booking_add(spc_id: int):
     if request.method == "POST":
-        
         return create_booking(request.form, spc_id)
     return new_booking_page(spc_id)
 
@@ -43,3 +44,19 @@ def booking_edit(bk_id: int):
 @role_required(["admin"])
 def booking_delete(bk_id: int):
     return delete_booking(bk_id)
+
+@booking_routes.route("/confirm/<int:bk_id>", methods=["GET", "POST"])
+@login_required
+@role_required(["admin", "cs"])
+def booking_confirm(bk_id: int):
+    if request.method == "POST":
+        return confirm_booking(bk_id)
+    return confirm_booking_page(bk_id)
+
+@booking_routes.route("/decline/<int:bk_id>", methods=["GET", "POST"])
+@login_required
+@role_required(["admin", "cs"])
+def booking_decline(bk_id: int):
+    if request.method == "POST":
+        return decline_booking(bk_id)
+    return decline_booking_page(bk_id)
